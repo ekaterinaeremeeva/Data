cfdc6482447a80246c6628cf2eafd3d775740c9d
Ekaterina <perovakatharina@gmail.com>
2023-11-03 17:48:04 +0300

    Решена задача №1

---
+++ js/1.js
@@ -0,0 +1,10 @@
+
+function task1()
+{
+function getWeekDay(date) {
+    let days = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+    return days[date.getDay()];
+    }
+let date = new Date (prompt("Введите дату в формате: год-месяц-число"));
+alert( getWeekDay(date) );
+}
